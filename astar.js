5add95cd6c9e1c4716a416ce49c66313ef9a7f85
Christian Sandven <christian@recheckit.com>
2019-05-28 16:22:56 +0200

    Update to es6

--- astar.js
+++ astar.js
@@ -1,144 +1,189 @@
-function Vector (x,y) {
-	this.x = x
-	this.y = y
+class Vector {
+
+    constructor (x,y) {
+        this.x = x
+        this.y = y
+    }
 }
 
-function Node (parent, vector, worldWidth) {
-	this.parent = parent
-	this.x = vector.x
-	this.y = vector.y
-	this.worldWidth = worldWidth
-	this.value = this.x + (this.y * worldWidth)
-	this.f = 0 // the cost to get to this node from the start
-	this.g = 0 // The cost to get from this node to the goal
-}
-Node.prototype.get = function () { 
-	return this
-}
-Node.prototype.calculateCost = function (costs) {
-	this.f = costs.f
-	this.g = costs.g
-}
+class Node {
 
-function aStar (worldWidth, worldHeight) { 
-	this.world = [[]]
-	this.worldWidth = worldWidth
-	this.worldHeight = worldHeight
-	this.currentPath = []
+    constructor (parent, vector, worldWidth) {
+        this.parent = parent
+        this.x = vector.x
+        this.y = vector.y
+        this.worldWidth = worldWidth
+        this.value = this.x + (this.y * worldWidth)
+        this.f = 0 // the cost to get to this node from the start
+        this.g = 0 // The cost to get from this node to the goal
+    }
 
-	this.createEmpty()
-}
-aStar.prototype.createEmpty = function() { 
-	for (var x = 0; x < this.worldWidth; x++) {
-		this.world[x] = []
-		for (var y = 0; y < this.worldHeight; y++) {
-			this.world[x][y] = 0
-		}
-	}
-}
-aStar.prototype.setWalls= function(walls) {
-	walls.forEach(w => this.world[w.pos.x][w.pos.y] = 1)
-}
-aStar.prototype.setStartPosition = function(x,y) { 
-	this.startPosition = new Vector(x,y)
-}
-aStar.prototype.setTargetPosition = function(x,y) { 
-	this.targetPosition = new Vector(x,y)
-}
-aStar.prototype.getPath= function() {
-	var maxTries = 10
-	while (this.currentPath.length === 0 && maxTries > 0) {
-		this.currentPath = this.findPath()
-		maxTries--
-	}
-	return this.currentPath
-}
-aStar.prototype.findPath = function () {
-	var worldSize = this.worldWidth * this.worldHeight,
-		pathStart = new Node(null, this.startPosition, this.worldWidth),
-		pathStop = new Node(null, this.targetPosition, this.worldWidth),
-		grid = new Array(worldSize),
-		openNodes = [pathStart.get()],
-		closedNodes = [],
-		path = []
+    calculateCost (costs) {
+        this.f = costs.f
+        this.g = costs.g
+    }
 
-	while(openNodes.length) {
-		var tmpPath = null,
-			max = worldSize,
-			min = -1
+    toVector () {
+        return new Vector(this.x, this.y)
+    }
 
-		openNodes.forEach((node, key) => {
-			if (node.f < max) {
-				max = node.f
-				min = key
-			}
-		})
-
-		// grab the next node and remove it from open nodes
-		curNode = openNodes.shift()
-
-		if (curNode.value === pathStop.value) { // The end of the path
-			tmpPath = closedNodes[closedNodes.push(curNode) - 1]
-
-			do {
-				path.push(new Vector(tmpPath.x, tmpPath.y))
-			} while (tmpPath = tmpPath.parent)
-
-			grid = closedNodes = openNodes = []
-			path.reverse() // reverse the array to get the correct pathline
-		} else { // not the target destination
-
-			// Loop through the node's neighbours
-			this.getNeighbours(curNode).forEach(neighbour => {
-				tmpPath = new Node(curNode, neighbour, this.worldWidth).get()
-				if (!grid[tmpPath.value]) {
-					tmpPath.calculateCost((function (currentNode, neighbour, tempPath, pathStop, astar) {
-						return {
-							g: currentNode.g + astar.manhattanDistance(neighbour, currentNode), // cost for the route this far
-							f: tempPath.g + astar.manhattanDistance(neighbour, pathStop.get()) // cost for entire route
-						}
-					})(curNode, neighbour, tmpPath, pathStop, this))
-
-					openNodes.push(tmpPath)
-					grid[tmpPath.value] = true // mark this node in the world graph as visited
-				}
-			})
-
-			closedNodes.push(curNode)
-		}
-	}
-	path.shift() // remove the first element as it is the current position
-	return path
-}
-aStar.prototype.manhattanDistance = function (point, goal) {
-	var	abs = Math.abs
-	return abs(point.x - goal.x) + abs(point.y - goal.y)
-}
-aStar.prototype.canWalkHere = function (x, y) {
-	return (this.world[x] !== null && this.world[x][y] !== null && this.world[x][y] < 1)
-}
-aStar.prototype.getNeighbours = function (node) {
-	var	neighbours = [],
-		north = node.y - 1,
-		south = node.y + 1,
-		east = node.x + 1,
-		west = node.x - 1
-
-	if(north > -1 && this.canWalkHere(node.x, north))
-		neighbours.push(new Vector(node.x, north))
-
-	if(east < this.worldWidth && this.canWalkHere(east, node.y))
-		neighbours.push(new Vector(east, node.y))
-
-	if(south < this.worldHeight && this.canWalkHere(node.x, south))
-		neighbours.push(new Vector(node.x, south))
-
-	if(west > -1 && this.canWalkHere(west, node.y))
-		neighbours.push(new Vector(west, node.y))
-
-	return neighbours
 }
 
-function Wall (x,y) {
-	this.pos = new Vector(x,y)
+class Astar {
+    
+    constructor (worldWidth, worldHeight) {
+        this.world = []
+        this.worldWidth = worldWidth
+        this.worldHeight = worldHeight
+
+        this.createEmpty()
+    }
+
+    /**
+     * Creates the outline of the worldmap
+     */
+    createEmpty () {
+        for (var y = 0; y < this.worldHeight; y++) {
+            this.world[y] = []
+            for (var x = 0; x < this.worldWidth; x++) {
+                this.world[y][x] = 0
+            }
+        }
+    }
+
+    /**
+     * Sets collision map
+     * 
+     * @param obstacles  
+     */
+    setObstacles (obstacles) {
+        for (let obstacle of obstacles) {
+            this.world[obstacle.y][obstacle.x] = 1
+        }
+    }
+
+    /**
+     * Gets path between two vectors with 10 attempts
+     * 
+     * @param fromPos 
+     * @param toPos 
+     */
+    getPath (fromPos, toPos) {
+        // Try a maximum of 10 times to get the path
+        var builtPath = [],
+            maxTries = 10
+        while (builtPath.length === 0 && maxTries > 0) {
+            builtPath = this._findPath(fromPos, toPos)
+            maxTries--
+        }
+        return builtPath
+    }
+
+    /**
+     * Finds the path between two points
+     * 
+     * @param {x, y} fromPos 
+     * @param {x, y} toPos 
+     */
+    _findPath (fromPos, toPos) {
+        const worldSize = this.worldWidth * this.worldHeight,
+              pathStartNode = new Node(null, fromPos, this.worldWidth),
+              pathStopNode = new Node(null, toPos, this.worldWidth),
+              grid = new Array(worldSize),
+              openNodes = [pathStartNode],
+              closedNodes = [],
+              path = []
+        
+        while (openNodes.length) {
+            const curNode = openNodes.shift()
+
+            // Check if the current node is at the end of the path
+            if (curNode.value === pathStopNode.value) {
+                var end = closedNodes[closedNodes.push(curNode) - 1]
+
+                do {
+                    path.push(end.toVector())
+                } while (end = end.parent)
+
+                grid.length = closedNodes.length = openNodes.length = 0
+                path.reverse()
+            } else {
+                const neighbours = this.getNeighbours(curNode)
+                for (let neighbour of neighbours) {
+                    const nNode = new Node(curNode, neighbour, this.worldWidth)
+                    if (!grid[nNode.value]) {
+                        nNode.calculateCost({
+                            g: curNode.g + this.manhattanDistance(neighbour, curNode),
+                            f: nNode.g + this.manhattanDistance(neighbour, pathStopNode)
+                        })
+
+                        openNodes.push(nNode)
+
+                        // mark this node in the world graph as visited
+                        grid[nNode.value] = true
+                    }
+                }
+
+                closedNodes.push(curNode)
+            }
+        }
+
+        // remove the first element as it is the current position
+        path.shift()
+        return path
+    }
+
+    /**
+     * Calculate manhattan distance between two nodes
+     * @param point 
+     * @param goal 
+     */
+    manhattanDistance (point, goal) {
+        return Math.abs(point.x - goal.x) + Math.abs(point.y + goal.y)
+    }
+    
+    /**
+     * Gets neighbours of a node
+     * @param node 
+     */
+    getNeighbours (node) {
+        const neighbours = [],
+              positionsToCheck = [
+                  new Vector(node.x, node.y - 1), // North
+                  new Vector(node.x + 1, node.y), // West
+                  new Vector(node.x, node.y + 1), // South
+                  new Vector(node.x - 1, node.y), // East
+              ]
+
+        for (let pos of positionsToCheck) {
+            if (pos.y >= 0 && pos.y < this.worldHeight && pos.x >= 0 && pos.x < this.worldWidth && this.world[pos.y][pos.x] === 0) {
+                neighbours.push(pos)
+            }
+        }
+
+        return neighbours
+    }
+
+    graph (path) {
+        const lines = []
+        for (let y = 0; y < this.worldHeight; y++) {
+            const line = []
+            for (let x = 0; x < this.worldWidth; x++) {
+                if (this.world[y][x] === 1) {
+                    line.push`X`
+                } else {
+                    const pathTrail = path.find(trail => trail.x === x && trail.y === y)
+                    if (pathTrail)
+                        line.push`#`
+                    else
+                        line.push`O`
+                }
+            }
+
+            lines.push(line.join``)
+        }
+
+        return lines.join("\n")
+    }
+
 }
